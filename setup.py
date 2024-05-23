7394ec6ad0daa8cdf4ba660080fbb071992293eb
parisa baqerzade <parisa.baqerzade@gmail.com>
2024-05-23 22:04:13 +0330

    update setup

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ def pre_install():
     return text
 
 setup(
-    name="face-identification",
+    name="face_identification",
     version="1.0.0", 
     author="Parisa Bagherzadeh", 
     description="A package to identify faces on an image",
     long_description=pre_install(),
     install_requires=['opencv-python', 'numpy', 'matplotlib', 'insightface'],
     author_email="parisa.baqerzade@gmail.com",
-    packages=["face-identification"]
+    packages=["face_identification"]
 )
